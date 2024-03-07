585da24f737fee8c22c72523969a3fbbbc96456f
Songded taweeporn <songded.tad@mtel.co.th>
2024-03-07 10:01:22 +0700

    issue type index

--- example/issue_type.ts
+++ example/issue_type.ts
@@ -15,6 +15,10 @@ async function test(): Promise<void> {
     // NOTE: This is a sample code to delete issue type
     response = await OBBMSSDK.client.issueTypesUpdate(data[0].id,{deleted_at: new Date().toISOString()})
     data = response.data.data
+
+    // NOTE: This is a sample code to get issue type
+    response = await OBBMSSDK.client.issueTypesIndex()
+    data = response.data.data
   
     console.dir({ data }, { depth: null })
 }
